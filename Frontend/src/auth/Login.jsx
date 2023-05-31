5a8a53f6e324759e61d34f465c6b001951a30ddf
ivan mufid <ivanmohammad2355@gmail.com>
2023-05-31 08:26:06 +0700

    login function

--- Frontend/src/auth/Login.jsx
+++ Frontend/src/auth/Login.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+import Layout from "../components/layout/Layout";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/v1/auth/login", {
+        email,
+        password,
+      });
+      if (res && res.data.success) {
+        toast.success(res.data.message);
+        navigate("/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong");
+    }
+  };
+
+  return (
+    <Layout title={"Login"}>
+      <div className="register">
+        <h2>Login Form</h2>
+        <div className="container register-container">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label className="form-label">Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control"
+                placeholder="Enter Your Mail"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control"
+                placeholder="Enter Your Password"
+                required
+              />
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn register-btn">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Login;
--- Frontend/src/auth/Register.jsx
+++ Frontend/src/auth/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Layout from "../components/layout/Layout";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import toast from "react-hot-toast";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -40,72 +40,70 @@ const Register = () => {
       <Layout title={"Register"}>
         <div className="register">
           <h2>Register Form</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label className="form-label">Name</label>
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="form-control"
-                placeholder="Enter Your Name"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Email</label>
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="form-control"
-                placeholder="Enter Your Mail"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Password</label>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="form-control"
-                placeholder="Enter Your Password"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Phone</label>
-              <input
-                type="number"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                className="form-control"
-                placeholder="Enter Your Phone"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Address</label>
-              <input
-                type="text"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                className="form-control"
-                placeholder="Enter You Address"
-                required
-              />
-            </div>
-            <div className="d-grid">
-              <button
-                type="submit"
-                className="btn value={name}register
-              onChange={(e) => setName(e.target.value)}-btn"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+          <div className="container register-container">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label className="form-label">Name</label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="form-control"
+                  placeholder="Enter Your Name"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Email</label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="form-control"
+                  placeholder="Enter Your Mail"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Password</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="form-control"
+                  placeholder="Enter Your Password"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Phone</label>
+                <input
+                  type="number"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  className="form-control"
+                  placeholder="Enter Your Phone"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Address</label>
+                <input
+                  type="text"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  className="form-control"
+                  placeholder="Enter You Address"
+                  required
+                />
+              </div>
+              <div className="d-grid">
+                <button type="submit" className="btn register-btn">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </Layout>
     </div>
